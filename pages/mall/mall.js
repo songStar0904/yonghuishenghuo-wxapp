468fb15c45d5a105fbc9657dab9067298cd0c467
songstar <1043328710@qq.com>
2018-07-27 13:54:37 +0800

    完成配送商城页面

--- pages/mall/mall.js
+++ pages/mall/mall.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    address: '定位中。。。',
+    address: '定位中...',
     seller: [{
       id: 1,
       name: '永辉超市',
@@ -24,13 +24,86 @@ Page({
       name: '永辉超市',
       icon: '../../images/seller_1.jpg'
     },
-    open_seller_list: false
+    current_breed_list: undefined,
+    current_breed: {},
+    breed: [{
+      sid: 1,
+      list: [{
+        id: 1,
+        name: '推荐'
+      }, {
+        id: 2,
+        name: '猜你喜欢',
+        new: true
+      }, {
+        id: 3,
+        name: '热销爆款'
+      }, {
+        id: 4,
+        name: '品质定制'
+      }, {
+        id: 5,
+        name: '环球美食'
+      }, {
+        id: 6,
+        name: '缤纷美食'
+      }, {
+        id: 7,
+        name: '热销爆款'
+      }, {
+        id: 8,
+        name: '品质定制'
+      }, {
+        id: 9,
+        name: '环球美食'
+      }, {
+        id: 10,
+        name: '缤纷美食'
+      }]
+    }, {
+      sid: 2,
+      list: [{
+        id: 1,
+        name: '推荐'
+      }, {
+        id: 2,
+        name: '猜你喜欢'
+      }, {
+        id: 3,
+        name: '热销爆款'
+      }, {
+        id: 4,
+        name: '品质定制',
+        new: true
+      }, {
+        id: 5,
+        name: '鱼类肉类'
+      }, {
+        id: 6,
+        name: '优良干货'
+      }, {
+        id: 7,
+        name: '中外名酒'
+      }, {
+        id: 8,
+        name: '品质定制'
+      }, {
+        id: 9,
+        name: '环球美食'
+      }, {
+        id: 10,
+        name: '缤纷美食'
+      }]
+    }],
+    open_seller_list: false,
+    goods: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    this.changeBreed()
     wx.authorize({
       scope: 'scope.userLocation',
       success: (res) => {
@@ -41,11 +114,66 @@ Page({
       }
     })
   },
+  // 获取商品
+  getGoods: function() {
+    wx.showLoading({
+      title: '拼命加载中...',
+    })
+    let that = this
+    let goods = this.randomGoods()
+    setTimeout(() => {
+      that.setData({
+        goods
+      })
+      wx.hideLoading()
+    }, 500)
+  },
+  // 随机生商品
+  randomGoods: function() {
+    let data = [{
+      name: '蒜蓉贝类拼盘',
+      price: 99,
+      icon: '../../images/goods/goods_1.jpg',
+      tab_price: true
+    }, {
+      name: '乐事薯片',
+      price: 5,
+      icon: '../../images/goods/goods_2.jpg',
+      tab_price: false
+    }]
+    let goods =[]
+    for(let i = 0; i < Math.random() * 10; i++) {
+      goods.push(data[Math.random() > 0.5 ? 0 : 1]);
+    }
+    return goods
+  },
   // 开关商家列表
   openSeller: function() {
     this.setData({
       open_seller_list: !this.data.open_seller_list
     })
+  },
+  // 改变品种
+  changeBreed: function() {
+    let breed = this.data.breed.filter((item) => {
+      return item.sid === this.data.current_seller.id
+    })
+    this.setData({
+      current_breed_list: breed[0].list
+    })
+    this.setData({
+      current_breed: this.data.current_breed_list[0]
+    })
+    this.getGoods()
+  },
+  // 选择品种
+  chooseBreed: function(e) {
+    console.log('sss')
+    let breed = e.currentTarget.dataset.item
+    this.setData({
+      current_breed: breed
+    })
+    this.getGoods()
   },
   // 选择商家
   selectSeller: function(e) {
@@ -53,8 +181,12 @@ Page({
       current_seller: e.currentTarget.dataset.item,
       open_seller_list: false
     })
+    this.changeBreed()
   },
   getLocation: function() {
+    wx.showLoading({
+      title: '定位中...',
+    })
     let that = this
     wx.getLocation({
       type: 'wgs84',
@@ -74,6 +206,7 @@ Page({
             that.setData({
               address: data
             })
+            wx.hideLoading()
           },
           fail: (res) => {
             console.log(res)
